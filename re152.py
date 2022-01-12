a6a3c9a89e8015c4e290e94f6296f3b3f6aa542b
KwangJe Moon <iqboy963295@gmail.com>
2022-01-12 20:24:30 +0900

    dfs/bfs

---
+++ re152.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+n,m = map(int, input())
+graph = []
+for i in range(m) :
+    graph.append(list(map(int,input())))
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+
+    while queue:
+        x,y=queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= m or ny < 0 or ny >= n :
+                continue
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx,ny))
+    return graph[n-1][m-1]
+print(bfs(0,0))
+
