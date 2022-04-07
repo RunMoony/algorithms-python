4d8b5ea6742c869471cea0b55f978a65c0c4ca8e
KwangJe Moon <iqboy963295@gmail.com>
2022-04-07 21:16:00 +0900

    파일명 정렬

---
+++ 프로그래머스/file_sort.py
@@ -0,0 +1,20 @@
+def solution(files):
+    answer = []
+    for file in files:
+        head = ''
+        number = ''
+        tail = ''
+        number_check = False
+        for i in range(len(file)):
+            if file[i].isdigit():
+                number += file[i]
+                number_check = True
+            elif not number_check:
+                head += file[i]
+            else:
+                tail = file[i:]
+                break
+        answer.append((head,number,tail))
+    answer.sort(key=lambda x: (x[0].upper(), int(x[1])))
+    return[''.join(t) for t in answer]
+
