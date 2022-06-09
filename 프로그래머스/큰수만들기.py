6e1ddb59777b7881b226ad9d9c52d02dc55312e6
RunMoony <iqboy963295@gmail.com>
2022-06-09 23:18:27 +0900

    큰수 만들기

--- 프로그래머스/큰수만들기.py
+++ 프로그래머스/큰수만들기.py
@@ -2,6 +2,7 @@
 
 from itertools import combinations
 
+## 내 코드
 def solution(number, k):
     answer = list(number)
     cnt = [i for i in range(len(number))]
@@ -13,3 +14,15 @@ def solution(number, k):
             a = a + answer[j]
         result.append(a)
     return max(result)
+
+## 참고한 코드
+
+def solution(number,k):
+    answer = []
+    for num in number:
+        while answer and k>0 and answer[-1] < num:
+            answer.pop()
+            k -= 1
+        answer.append(num)
+    answer = ''.join(answer[:len(number)-k])
+    return answer
